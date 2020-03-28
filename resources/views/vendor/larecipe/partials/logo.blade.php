4944d99ca9456a3355e6408481a0e5d132d96ed8
Matthew Kilgore <tankerkiller125@gmail.com>
2020-03-27 22:08:55 -0400

    Update docs logo

--- resources/views/vendor/larecipe/partials/logo.blade.php
+++ resources/views/vendor/larecipe/partials/logo.blade.php
@@ -7,8 +7,8 @@
     xmlns="http://www.w3.org/2000/svg"
     version="1.1"
     id="svg3713"
-    width="180"
-    height="96"
+    width="100"
+    height="50"
     viewBox="0 0 180 96">
     <metadata
         id="metadata3719">
